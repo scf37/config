6df8ea23a9c17a6d4be9cf9b0fcc3b754f04f8d5
scf37 <scf37@github>
2013-06-01 11:21:34 +0400

    removed junk

--- src/main/java/ru/scf37/config/ConfigException.java
+++ src/main/java/ru/scf37/config/ConfigException.java
@@ -1,9 +1,5 @@
 package ru.scf37.config;
 
-import java.util.Properties;
-
-import ru.scf37.config.impl.prop.PropertiesConfigBuilder;
-
 /**
  * Exception thrown by Config library in case of any problems
  * 
@@ -19,12 +15,5 @@ public class ConfigException extends RuntimeException {
 
 	public ConfigException(String message) {
 		super(message);
-		
-		ConfigReader<Properties> reader = ConfigFactory.readPropertiesFrom("classpath:").build();
-		reader.read(null, null, null, "my.properties");
-		
 	}
-	
-	
-
 }
