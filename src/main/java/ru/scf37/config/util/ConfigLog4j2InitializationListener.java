adfbfff317379fd45216a5b855847e0fa82151e4
scf37 <scf37@github>
2013-10-15 00:16:57 +0400

    Log4j2 - better initialization

--- src/main/java/ru/scf37/config/util/ConfigLog4j2InitializationListener.java
+++ src/main/java/ru/scf37/config/util/ConfigLog4j2InitializationListener.java
@@ -1,5 +1,9 @@
 package ru.scf37.config.util;
 
+import javax.servlet.ServletContextEvent;
+
+import org.apache.logging.log4j.core.web.Log4jServletContextListener;
+
 /**
  * Performs log4j2 initialization for web applications
  * 
@@ -10,10 +14,21 @@ package ru.scf37.config.util;
  *
  */
 public class ConfigLog4j2InitializationListener extends AbstractConfigLog4jInitializationListener {
-	
+	private Log4jServletContextListener log4jServletContextListener = new  Log4jServletContextListener();
 	@Override
 	public AbstractLog4jConfigurer createConfigurer() {
 		return new ConfigLog4j2Configurer();
+	}
+	
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		super.contextInitialized(sce);
+		log4jServletContextListener.contextInitialized(sce);
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		log4jServletContextListener.contextDestroyed(sce);
 	}
 
 }
